b9c05c4c85016a60d26e7a8aaeab912d507fbfb6
Sean Kelley <seankly@gmail.com>
2021-09-14 13:15:48 -0400

    Add storage struct
    Bulls and cows now are stored in a struct, finished basic gameplay loop

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -6,6 +6,8 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
+    FBullCowCount BullCowCount;
+
     Isograms = GetValidWords(Words);
 
     SetupGame();
@@ -85,12 +87,10 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
         return;
     }
 
-    // Show the player Bulls and Cows
-    int32 Bulls;
-    int32 Cows; 
-    GetBullCows(Guess, Bulls, Cows);
+    // Show the player Bulls and Cows 
+   FBullCowCount Score = GetBullCows(Guess);
 
-    PrintLine(TEXT("You have %i Bulls and %i Cows", Bulls, Cows));
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
 
     PrintLine(TEXT("Guess again, you have %i lives left"), Lives);
 }
@@ -126,16 +126,14 @@ TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList
     return ValidWords;
 }
 
-void UBullCowCartridge::GetBullCows(const FString& Guess, int32& BullCount, int32& CowCount) const
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
 {
-    BullCount = 0;
-    CowCount = 0;
-
+    FBullCowCount Count;
     for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); GuessIndex++)
     {
         if (Guess[GuessIndex] == HiddenWord[GuessIndex])
         {
-            BullCount++;
+            Count.Bulls++;
             continue; 
         }
         
@@ -143,9 +141,10 @@ void UBullCowCartridge::GetBullCows(const FString& Guess, int32& BullCount, int3
         {
             if (Guess[GuessIndex] == HiddenWord[IndexHidden])
             {
-                CowCount++;
+                Count.Cows++;
                 break;
             }
         }
     }
+    return Count;
 }
